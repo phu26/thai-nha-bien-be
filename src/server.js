b810fbfa4c98088108b5bb973f1c5a9173f52872
phu26 <44081554+phu26@users.noreply.github.com>
2023-08-19 02:54:11 +0700

    ưdw

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 import  express from "express";
 
 import bodyParser from 'body-parser';
-import productData from "./data/Products.js";
+
 import dotenv from 'dotenv';
 import cors from 'cors';
 import conncectDatabase from "./config/MongoDb.js";
 import productss from './routers/products.js';
 import catalog from './routers/catalog.js';
-import multer  from "multer";
+
 import category from './routers/category.js';
 
 dotenv.config();
@@ -21,12 +21,7 @@ app.use(bodyParser.urlencoded({extended: true, limit: '30mb'}));
 app.use(cors());
 
 
-app.get("/api/SanPham",(req,res)=>{
-    res.json(productData.getAllProducts());
-}) 
-app.get("/api/products/:id",(req,res)=>{
-    res.json(productData.getProductById(req.params.id));
-}) 
+
 app.get("/",(req,res)=>{
 
     res.send("API running....");
